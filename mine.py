513e09c2c89e84aee26a6553293fb12cc11b53c5
Brianmax <31070682+Brianmax@users.noreply.github.com>
2022-01-25 15:17:06 -0500

    Cambiar de texturas

--- mine.py
+++ mine.py
@@ -25,6 +25,19 @@ stone_texture = load_texture('assets/stone_block.png')
 dirt_texture = load_texture('assets/dirt_block.png')
 brick_texture = load_texture('assets/brick_block.png')
 
+block_pick = 1
+
+def update():
+    global block_pick
+    if held_keys['1']:
+        block_pick = 1
+    if held_keys['2']:
+        block_pick = 2
+    if held_keys['3']:
+        block_pick = 3
+    if held_keys['4']:
+        block_pick = 4
+
 class Voxel(Button):
     def __init__(self, position=(0,0,0), texture = grass_texture):
         super().__init__(
@@ -40,7 +53,14 @@ class Voxel(Button):
     def input(self, tecla):
         if self.hovered:
             if tecla == "left mouse down":
-                Voxel(position = self.position + mouse.normal)
+                if block_pick == 1:
+                    Voxel(position=self.position+mouse.normal, texture=grass_texture)
+                if block_pick == 2:
+                    Voxel(position=self.position + mouse.normal, texture=stone_texture)
+                if block_pick == 3:
+                    Voxel(position=self.position + mouse.normal, texture=brick_texture)
+                if block_pick == 4:
+                    Voxel(position=self.position + mouse.normal, texture=dirt_texture)
 
 
 
