7e118fb3e8f6977713f84c22a928e4b174d0a918
Brianmax <31070682+Brianmax@users.noreply.github.com>
2022-05-05 15:32:28 -0700

    IA

--- mine.py
+++ mine.py
@@ -1,74 +1,96 @@
-from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
-"""
-class Boton_prueba(Button):
-    def __init__(self):
-        super().__init__(
-            model = "circle",
-            texture = "brick",
-            color = color.red,
-            highlight_color = color.blue,
-            pressed_color = color.green
-        )
+# -------------------------------------------------------------------------
+# Crack the Code
+# Inteligencia Artificial con Python
+# Sesion 2 - Captura de datos
+# -------------------------------------------------------------------------
+# Importar bibliotecas que se utilizarán - no modifiques esta sección
+import cv2
+import os
+import imutils
+from camera import getcamera
 
-    def input(self, tecla):
-        if self.hovered:
-            if tecla == "left mouse down":
-                print("Hola")
-test = Boton_prueba()
-"""
+# Crear carpeta de persona:
+print('Escribe tu nombre: ')
+personName = input()
+dataPath = './data'
+personPath = dataPath + '/' + personName
 
-app = Ursina()
+# Muestra acción a tomar dependiendo del nombre agregado
+if os.path.exists(personPath):
+    print('Persona ya registrada, sobreescribiendo datos...')
+else:
+    os.makedirs(personPath)
+    print('Nueva persona, capturando datos...')
 
-grass_texture = load_texture('assets/grass_block.png')
-stone_texture = load_texture('assets/stone_block.png')
-dirt_texture = load_texture('assets/dirt_block.png')
-brick_texture = load_texture('assets/brick_block.png')
+# -------------------------------------------------------------------------
+# Escribe tu código aquí:
 
-block_pick = 1
+# Actividad 0: instalar imutils
+# pip install imutils
 
-def update():
-    global block_pick
-    if held_keys['1']:
-        block_pick = 1
-    if held_keys['2']:
-        block_pick = 2
-    if held_keys['3']:
-        block_pick = 3
-    if held_keys['4']:
-        block_pick = 4
+# Actividad 1: copia el codigo de la sesion 1
 
-class Voxel(Button):
-    def __init__(self, position=(0,0,0), texture = grass_texture):
-        super().__init__(
-            parent = scene,
-            position = position,
-            model = "assets/block",
-            origin_y = 0.5,
-            texture = texture,
-            color = color.white,
-            scale = 0.5
-        )
+# Abrir camara web
+camera = getcamera()
+cap = cv2.VideoCapture(camera, cv2.CAP_DSHOW)
 
-    def input(self, tecla):
-        if self.hovered:
-            if tecla == "left mouse down":
-                if block_pick == 1:
-                    Voxel(position=self.position+mouse.normal, texture=grass_texture)
-                if block_pick == 2:
-                    Voxel(position=self.position + mouse.normal, texture=stone_texture)
-                if block_pick == 3:
-                    Voxel(position=self.position + mouse.normal, texture=brick_texture)
-                if block_pick == 4:
-                    Voxel(position=self.position + mouse.normal, texture=dirt_texture)
+# Carga el archivo para utilizar el detector de rostros
+faceClassif = cv2.CascadeClassifier("rostros.xml")
 
+# Actividad 3a: Inicia contador (para set de entrenamiento)
+contador = 0
 
+# Ciclo para tomar fotos de forma continua
+while True:
+    # Tomar fotografía
+    ret, frame = cap.read()
 
-for z in range(20):
-    for x in range(20):
-        voxel = Voxel(position=(x,0,z))
+    # Comprobar que exista una imagen
+    if not ret:
+        break
 
-player = FirstPersonController()
+    # Actividad 2: Cambia el tamaño de la foto tomada
+    frame = imutils.resize(frame, width=640)
 
+    # Crea una imagen en escala de grises a partir de la foto
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-app.run()
+    # Utiliza el detector en la imagen de escala de grises
+    faces = faceClassif.detectMultiScale(gray,
+                                         scaleFactor=1.1,
+                                         minNeighbors=5,
+                                         minSize=(120, 120),
+                                         maxSize=(1000, 1000))
+
+    # Ciclo por cada rostro detectado en la imagen
+    for (x, y, w, h) in faces:
+        # Dibuja un rectángulo al rededor del rostro
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Actividad 4a: Crea una copia de la imagen
+        auxFrame = frame.copy()
+
+        # Actividad 4b: Obtiene el recuadro del rostro
+        rostro = auxFrame[y:y + h, x:x + w]
+        rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+
+        # Actividad 5: Guarda el rostro como imagen
+        cv2.imwrite(personPath + '/rotro_{}.jpg'.format(contador), rostro)
+        print('rotro_{}.jpg'.format(contador) + ' guardado')
+
+        # Actividad 3b: Incrementa el contador de imagenes
+        contador = contador + 1
+
+    # Mostrar la imagen en pantalla
+    cv2.imshow('frame', frame)
+
+    # Espera a que toques una tecla para cerrar la ventana
+    # Actividad 3c: termina el programa cuando el contador llegue a 300
+    if contador >= 300 or cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# --------------------------------------------------------------------------
+# Cierra la cámara y las ventanas - no borres estas lineas
+# Deja estas lineas hasta abajo
+cv2.destroyAllWindows()
+cap.release()
