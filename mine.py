e23acc8f8da68cfe56a0d0ac3d7d714f386d4bd6
Brianmax <31070682+Brianmax@users.noreply.github.com>
2022-06-29 18:18:33 -0500

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,12 +1,20 @@
-local damage = 50
-local seconds = 10
-local mayTouch = true
+    kernel = np.ones((3,3), np.uint8)
+    dilatacion = cv2.dilate(bordes, kernel)
+    cv2.imshow('Dilatacion', dilatacion)
 
-script.Parent.Touched:connect(function(hit)
-	if hit and hit.Parent and hit.Parent:FindFirstChild("Humanoid") and mayTouch then
-		hit.Parent.Humanoid.Health -= damage 
-		mayTouch = false
-		wait(seconds)
-		mayTouch = true
-	end
-end)
+    #Camara y cambio de fondo
+
+    _, binaria = cv2.threshold(grises, 100, 255, cv2.THRESH_BINARY)
+    binaria = cv2.bitwise_not(binaria)
+
+    #separar la persona del fondo
+
+    persona = np.zeros(frame.shape, np.uint8)
+    for i in range(3):
+        persona[:, :, i] = cv2.bitwise_and(frame[:, :, i], binaria)
+    imshow('Persona', persona)
+
+    #Leer la imagen del fondo
+
+    fondo = cv2.imread("./img/fondo.jpg")
+    fondo = cv2.resize(fondo, binaria.shape[::-1])
