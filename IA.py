7da61cb3c89cc2085df2144c7753abcdee672eb0
Brianmax <31070682+Brianmax@users.noreply.github.com>
2022-07-18 19:49:46 -0500

    Update IA.py

--- IA.py
+++ IA.py
@@ -1,10 +1,10 @@
 import pygame
 
+
 class Game:
     screen = None
     aliens = []
-    lost = False
-    rockets = []
+    perdio = False
     def __init__(self, width, height):
         pygame.init()
         self.width = width
@@ -13,26 +13,18 @@ class Game:
         self.clock = pygame.time.Clock()
         done = False
         hero = Hero(self, width/2, height-20)
-        rocket = None
         generator = Generator(self)
         while not done:
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_LEFT]:
-                hero.x = hero.x - 2 if hero.x > 20 else 0
-            elif pressed[pygame.K_RIGHT]:
-                hero.x = hero.x + 2 if hero.x < width - 20 else 0
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     done = True
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not self.lost:
-                    self.rockets.append(Rocket(self,hero.x, hero.y))
 
             pygame.display.flip()
             self.clock.tick(60)
             self.screen.fill((0, 0, 0))
             for alien in self.aliens:
                 alien.draw()
-            if not self.lost:
+            if not self.perdio:
                 hero.draw()
 
 
@@ -45,9 +37,9 @@ class Alien:
 
     def draw(self):
         pygame.draw.rect(self.game.screen,
-                         (81, 43, 88), pygame.Rect(self.x, self.y, self.size, self.size))
+                         (81, 43, 88),
+                         pygame.Rect(self.x, self.y, self.size, self.size))
         self.y += 0.05
-
 
 class Generator:
     def __init__(self, game):
@@ -57,24 +49,21 @@ class Generator:
             for y in range(margin, int(game.height / 2), width):
                 game.aliens.append(Alien(game, x, y))
 
-class Rocket:
-    def __init__(self,game, x, y):
-        self.x = x
-        self.y = y
-        self.game = game
-    def draw(self):
-        pygame.draw.rect(self.game.screen,(254, 52, 110), pygame.Rect(self.x, self.y, 2, 4))
-        self.y -= 2
 
 class Hero:
     def __init__(self, game, x, y):
         self.x = x
         self.game = game
         self.y = y
-
     def draw(self):
-        pygame.draw.rect(self.game.screen,(210, 250, 251),pygame.Rect(self.x, self.y, 8, 5))
+        pygame.draw.rect(self.game.screen, (210,250, 251), pygame.Rect(self.x, self.y, 8,5))
 
-
+class Rocket:
+    def __init__(self, game, x, y):
+        self.x = x
+        self.y = y
+        self.game = game
+    def draw(self):
+        pygame.draw.rect(self.game.screen, (254, 52, 110), pygame.Rect(self.x, self.y, 2, 4))
 if __name__ == '__main__':
     game = Game(600, 400)
