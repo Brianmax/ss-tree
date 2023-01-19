b6e81a5bbf8f020fb1aa176b79d24152715f62f5
Brianmax <31070682+Brianmax@users.noreply.github.com>
2023-01-18 19:58:24 -0500

    Update IA.py

--- IA.py
+++ IA.py
@@ -1,133 +1,69 @@
-import pygame
+# -------------------------------------------------------------------------
+# Sesión 3: Entrenamiento
+
+# Tabla con los valores Q y parametros del entrenamiento
+# Escribe aquí tu codigo
 
 
-class Game:  
-    screen = None
-    aliens = []
-    rockets = []
-    lost = False
-    win = False
-    def __init__(self, width, height):
-        pygame.init()
-        self.width = width
-        self.height = height
-        self.screen = pygame.display.set_mode((width, height))
-        self.clock = pygame.time.Clock()
-        self.fondo = pygame.image.load("fondo.png")
-        done = False
-
-        pygame.mixer.music.load("song.wav")
-        pygame.mixer.music.play(-1)
-
-        hero = Hero(self, width / 2, height - 20)
-        generator = Generator(self)
-        rocket = None
-
-        while not done:
-            if len(self.aliens) == 0:
-                self.win = True
-                self.displayText("VICTORY ACHIEVED")
-
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_LEFT]:
-                hero.x -= 2 if hero.x > 20 else 0
-            elif pressed[pygame.K_RIGHT]:
-                hero.x += 2 if hero.x < width - 20 else 0
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    done = True
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    self.rockets.append(Rocket(self, hero.x, hero.y))
+# Es una función que nos ayuda a explorar nuevas posibilidades o a utilizar el conocimiento que ya tenemos
+# para ello utiliza el parametro explorar, el cual es un porcentaje que nos ayuda a decidir que tantas veces vamos
+# a utilizar valores al azar y cuantas veces vamos a usar las mejores respuestas que tenemos
+def siguiente_accion(fila_actual, columna_actual, explorar):
+    # Escribe aquí tu código
+    pass
 
 
-            pygame.display.flip()
-            self.clock.tick(60)
-            #self.screen.fill((0, 0, 0))
-            self.screen.blit(self.fondo, (0,0))
+# -------------------------------------------------------------------------
+# JUEGO - Este parte del código se modificará sesión a sesión
 
-            for alien in self.aliens:
-                alien.draw()
-                alien.checkCollision(self)
-                if (alien.y > height):
-                    self.lost = True
-                    self.displayText("YOU DIED")
+# Elegir punto inicial del juego al azar
+x, y = punto_inicial()
 
-            for rocket in self.rockets:
-                if not self.win: rocket.draw()
-                if rocket.y<=0:
-                    self.rockets.remove(rocket)
+# Ciclo para mantener el juego abierto y poder intefactuar con el
+while True:
+    # Accion al azar usando la función punto_siguiente
+    accion = np.random.randint(4)
 
-            if not self.lost: hero.draw()
+    # Calcular siguiente punto
+    x, y = punto_siguiente(x, y, accion)
 
-    def displayText(self, text):
-        pygame.font.init()
-        font = pygame.font.SysFont('Arial', 50)
-        textsurface = font.render(text, False, (44, 0, 62))
-        self.screen.blit(textsurface, (110, 160))
+    # Espera y fondo
+    sleep(0.4)  # Dejar mas tiempo para poder ver los resultados
+    ventana.fill((0, 0, 0))
 
+    # Diujar laberinto
+    dibujar_laberinto(x, y)
+    pygame.display.flip()
 
-class Alien:
-    def __init__(self, game, x, y):
-        self.x = x
-        self.game = game
-        self.y = y
-        self.size = 30
-        self.image = pygame.image.load("alien.png")
-
-    def draw(self):
-        #pygame.draw.rect(self.game.screen,
-                         #(81, 43, 88),
-                         #pygame.Rect(self.x, self.y, self.size, self.size))
-        self.game.screen.blit(self.image, (self.x,self.y))
-        self.y += 0.05
-
-    def checkCollision(self, game):
-        for rocket in game.rockets:
-            if (rocket.x < self.x + self.size and
-                    rocket.x > self.x - self.size and
-                    rocket.y < self.y + self.size and
-                    rocket.y > self.y - self.size):
-                game.rockets.remove(rocket)
-                game.aliens.remove(self)
+    # Condición del fin del juego
+    if fin_del_juego(x, y):
+        if recompensas[x, y] == 100:
+            print("¡Has ganado!")
+        else:
+            print("¡Has perdido!")
+        break
 
 
-class Hero:
-    def __init__(self, game, x, y):
-        self.x = x
-        self.game = game
-        self.y = y
-        self.image = pygame.image.load("nave2.png")
+# -------------------------------------------------------------------------
+# Sesión 4 - Resultados del entrenamiento
 
-    def draw(self):
-
-        #pygame.draw.rect(self.game.screen,(210, 250, 251),pygame.Rect(self.x, self.y, 8, 5))
-        self.game.screen.blit(self.image, (self.x,self.y))
-
-class Generator:
-    def __init__(self, game):
-        margin = 30
-        width = 50
-        for x in range(margin, game.width - margin, width):
-            for y in range(margin, int(game.height / 2), width):
-                game.aliens.append(Alien(game, x, y))
+# Define una función que va a elegir siempre el camino más corto entre un punto inicial y la meta
+def camino_mas_corto(inicio_x, inicio_y):
+    # Escribe aquí tu codigo
+    pass
 
 
+# Dibuja el camino más corto desde una posición hasta la meta
+def dibuja_camino_mas_corto(inicio_x, inicio_y):
+    # Escribe aquí tu codigo
+    pass
 
 
-class Rocket:
-    def __init__(self, game, x, y):
-        self.x = x
-        self.y = y
-        self.game = game
-
-    def draw(self):
-        pygame.draw.rect(self.game.screen,
-                         (254, 52, 110),
-                         pygame.Rect(self.x, self.y, 2, 4))
-        self.y -= 2
+# Prueba tu inteligencia artificial para resolver el laberinto desde varias posiciones iniciales
+# Escribe aquí tu codigo
 
 
-if __name__ == '__main__':
-    game = Game(600, 400)
-    
+# -------------------------------------------------------------------------
+# No borres esta linea, deja esto siempre hasta el final
+# Cierra el juego
+pygame.quit()
